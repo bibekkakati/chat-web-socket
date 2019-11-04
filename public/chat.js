192d3a12150b3db7039e1bccde2f1a51a8cd9453
Bibek <34815829+bibekkakati@users.noreply.github.com>
2019-11-04 23:17:24 +0530

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 //Make connection
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('https://bakbakchat.herokuapp.com');
 
 //Query DOM
 var message = document.getElementById('message');
